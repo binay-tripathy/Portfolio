f8435284811c5c49c3f84382644cb5c63f83d100
Binay Tripathy <binaytripathy123@gmail.com>
2023-08-22 18:36:16 +0530

    Minor Changes

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Portfolio.scss'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import Loader from 'react-loaders'
+
 
 const Portfolio = () => {
     return (
-        <div className='container portfolio-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters letterClass='text-animate' strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k']} idx={15} />
-                </h1>
+        <>
+            <div className='container portfolio-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters letterClass='text-animate' strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k']} idx={15} />
+                    </h1>
+                </div>
             </div>
-        </div>
+            <Loader type='ball-clip-rotate-multiple' />
+        </>
     )
 }
 
