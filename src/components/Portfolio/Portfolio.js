6541bd7c2b0d42bc064d6469de3a5c9affc83212
Binay Tripathy <binaytripathy123@gmail.com>
2023-08-23 19:43:41 +0530

    Added some pics

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,7 +2,10 @@ import React from 'react'
 import './Portfolio.scss'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import Loader from 'react-loaders'
-
+import image1 from '../../assets/images/Wetter.png';
+import image2 from '../../assets/images/Plantlet.png';
+import image3 from '../../assets/images/Grill&Chill.png';
+import image4 from '../../assets/images/ToDoList.png';
 
 const Portfolio = () => {
     return (
@@ -12,6 +15,20 @@ const Portfolio = () => {
                     <h1>
                         <AnimatedLetters letterClass='text-animate' strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k']} idx={15} />
                     </h1>
+                </div>
+                <div className="grid-container">
+                    <div className="grid-item">
+                        <a href="/"><img src={image1} alt="Wetter" /></a>
+                    </div>
+                    <div className="grid-item">
+                        <a href="/"><img src={image2} alt="Plantlet" /></a>
+                    </div>
+                    <div className="grid-item">
+                        <a href="/"><img src={image3} alt="Grill&Chill" /></a>
+                    </div>
+                    <div className="grid-item">
+                        <a href="/"><img src={image4} alt="ToDoList" /></a>
+                    </div>
                 </div>
             </div>
             <Loader type='ball-clip-rotate-multiple' />
