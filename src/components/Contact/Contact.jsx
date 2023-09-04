8b8d67edf275fac50f5b4ca4930a38fc348a68a7
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-04 17:20:22 +0530

    Added EmailJS in Contact Form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.scss'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import Loader from 'react-loaders'
 import ContactImg from '../../assets/images/Contact2.png'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+    const refForm = useRef()
+    const sendEmail = (e) =>{
+        e.preventDefault();
+        emailjs
+            .sendForm(
+                'service_f0y8so4',
+                'template_d4h6hay',
+                refForm.current,
+                'S6uqU59PY__Ccz6BU'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
     return (
         <>
             <div className="container contact-page">
@@ -16,7 +37,7 @@ const Contact = () => {
                         If you have any requests or questions, don't hesitate to contact me using below form either.
                     </p>
                     <div className="contact-form">
-                        <form>
+                        <form id='contact-form' ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
                                     <input placeholder="Name" type="text" name="name" required />
